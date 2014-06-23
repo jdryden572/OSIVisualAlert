3e26de1e3622c539c1ffaff96ae73f8f7ed15b37
jdryden572 <jdryden572@gmail.com>
2014-06-23 11:02:54 -0400

    woops, added comma

--- TSPhillyVisualAlert.py
+++ TSPhillyVisualAlert.py
@@ -20,7 +20,7 @@ config = {
 		'red': 			{'on': True, 'bri': 150, 'sat': 255, 'transitiontime': 4, 'xy': [0.8, 0.3]},
 		'orange': 		{'on': True, 'bri': 150, 'sat': 255, 'transitiontime': 4, 'xy': [0.7, 0.4]},
 		'yellow':		{'on': True, 'bri': 150, 'sat': 255, 'transitiontime': 4, 'xy': [0.55, 0.46]},
-		'greenYellow':	{'on': True, 'bri': 150, 'sat': 255, 'transitiontime': 4, 'xy': [0.7, 0.7]}
+		'greenYellow':	{'on': True, 'bri': 150, 'sat': 255, 'transitiontime': 4, 'xy': [0.7, 0.7]},
 		'green':		{'on': True, 'bri': 150, 'sat': 255, 'transitiontime': 4, 'xy': [0.5, 0.8]},
 		'allOn':		{'on': True, 'bri':  50, 'sat': 255, 'transitiontime': 2, 'ct': 250},
 		'noConnect':	{'on': True, 'bri': 150, 'sat': 255, 'transitiontime': 4, 'effect': 'colorloop'},
